03296a8dbbd376c8d169fe155fbe2ec834a71136
brkim <fried4chicken@kentech.ac.kr>
2025-08-25 20:26:57 +0900

    update

---
+++ temp.py
@@ -0,0 +1,12 @@
+import numpy
+import matplotlib
+import tensorflow
+import tqdm
+import sklearn
+
+
+print('numpy='+numpy.__version__)
+print('matplotlib='+matplotlib.__version__)
+print('tensorflow='+tensorflow.__version__)
+print('tqdm='+tqdm.__version__)
+print('scikit-learn='+sklearn.__version__)
